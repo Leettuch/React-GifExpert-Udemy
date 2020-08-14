6c5658dcba2ab02e020b0d5610a005e6c458dc24
Leettuch <e.andres.estay@gmail.com>
2020-08-14 16:25:13 -0400

    First Commit

---
+++ docs/precache-manifest.6173dee7256f5461877545bdd4a459a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd8d19b28a11baf979b52c4bd7d223c9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "505da32025c7f68e84a2",
+    "url": "/static/css/main.7c566318.chunk.css"
+  },
+  {
+    "revision": "e10966b3bd0a15600c1c",
+    "url": "/static/js/2.c4592ee5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c4592ee5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "505da32025c7f68e84a2",
+    "url": "/static/js/main.f233d761.chunk.js"
+  },
+  {
+    "revision": "4d3c5aad2ee9da455662",
+    "url": "/static/js/runtime-main.a4f11275.js"
+  }
+]);
